4f3352a1267436e89d69b41b473311de43ba8f63
Aditya Raj Singh <120239595+Adityainshort@users.noreply.github.com>
2025-04-14 15:59:10 +0530

    fixed fetching issue

--- scripts.js
+++ scripts.js
@@ -1,233 +1,132 @@
-console.log("Lets write javascript")
-let playsong = document.querySelector('#playsong')
-var prevsong = document.querySelector('#prevsong')
-var nextsong = document.querySelector('#nextsong')
-let songstracksrc = []
+console.log("Lets write javascript");
+let playsong = document.querySelector('#playsong');
+var prevsong = document.querySelector('#prevsong');
+var nextsong = document.querySelector('#nextsong');
 
+let songstracksrc = [
+    "/songs/Ami Je Tomar - Arjit Singh.mp3",
+    "/songs/Arjan Vailly - Bhupeinder Babbal.mp3",
+    "/songs/Hua Main - Raghav Chaitanya.mp3",
+    "/songs/Husn - Anuv Jain.mp3",
+    "/songs/Mere Dholna - Shreya Ghoshal.mp3",
+    "/songs/Papa Meri Jaan - Sonu Niagam.mp3",
+    "/songs/Saari Duniya Jalaa Denge - B Praak.mp3"
+];
 
-
-
-
-main()
-let currtracksrc ;
+let currtracksrc;
 var cuurentaudiotrack = new Audio();
+main();
 
-async function getsongs() {
+async function main() {
+    let songs = document.querySelector(".songs");
 
-    let a = await fetch(`./songs`)
-
-    let promise = await a.text();
-    console.log(promise)
-    
-    let div = document.createElement("div");
-    div.innerHTML = promise;
-    let as = div.getElementsByTagName("a");
-    let songs = []
-    for (let i = 0; i < as.length; i++) {
-        const element = as[i];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
-        }
-    }
-    return songs
-}
-
-
-async function main(axxxa) {
-    
-    let a = await getsongs(axxxa);
-    let songs = document.querySelector(".songs")
-    for (let song of a) {
-        // console.log(song)
-        song = song.split(`/songs/`)[1];
-        song = song.split(".mp3")[0];
-        song = song.replace(/%20/g, " ");
-
-        let [songname, artistName] = song.split(/\s+-\s+/);
-        console.log(songname, artistName);
-
-        let fullSongPath = `/songs/` + songname + " - " + artistName + ".mp3";
-        songstracksrc.push(fullSongPath);
+    for (let fullPath of songstracksrc) {
+        let filename = fullPath.split("/songs/")[1];
+        let decodedName = decodeURIComponent(filename).replace(".mp3", "");
+        let [songname, artistName] = decodedName.split(/\s+-\s+/);
 
         songs.innerHTML += `<div class="card">
-                        <img class="invert" src="img/song.svg" alt="">
-                        <span>
-                            <div class="songname">${song.split("-")[0]}</div>
-                            <div class="artistname">${song.split("-")[1]}</div>
-                        </span>
-                        <img class="invert" src="img/play.svg" alt="">
-                    </div>`
-
+            <img class="invert" src="img/song.svg" alt="">
+            <span>
+                <div class="songname">${songname}</div>
+                <div class="artistname">${artistName}</div>
+            </span>
+            <img class="invert" src="img/play.svg" alt="">
+        </div>`;
     }
-    currtracksrc = songstracksrc[0]
-    cuurentaudiotrack.src=songstracksrc[0];
 
-
+    currtracksrc = songstracksrc[0];
+    cuurentaudiotrack.src = songstracksrc[0];
 
     var cardcontainer = document.querySelectorAll('.card');
 
     for (const card of cardcontainer) {
-        card.addEventListener("click", element => {
+        card.addEventListener("click", () => {
             var spanElement = card.querySelector('span');
-
-            var songNameElement = spanElement.querySelector('.songname');
-            var artistNameElement = spanElement.querySelector('.artistname');
-
-            var songName = songNameElement.textContent.trim();
-            var artistName = artistNameElement.textContent.trim();
-            currtracksrc = `/songs/` + songName + " - " + artistName + ".mp3";
-            // console.log(currtracksrc)
+            var songName = spanElement.querySelector('.songname').textContent.trim();
+            var artistName = spanElement.querySelector('.artistname').textContent.trim();
+            currtracksrc = `/songs/${songName} - ${artistName}.mp3`;
             document.querySelector('.songinfoname').innerHTML = songName;
             playsongs(currtracksrc);
-            
-        })
-        
+        });
     }
-    
-    
-    
-    /// event on play pause next previous
-    
+
     playsong.addEventListener("click", () => {
         if (cuurentaudiotrack.paused) {
-            // cuurentaudiotrack.src=currtracksrc;
             cuurentaudiotrack.play();
             changsongname(currtracksrc);
-            playsong.src = "img/pause.svg"
-            console.log("song played")
-        }
-        else {
+            playsong.src = "img/pause.svg";
+            console.log("song played");
+        } else {
             cuurentaudiotrack.pause();
-            console.log("song paused")
-            playsong.src = "img/play.svg"
+            console.log("song paused");
+            playsong.src = "img/play.svg";
         }
-    })
+    });
 
     nextsong.addEventListener("click", () => {
         let curentindex = songstracksrc.indexOf(currtracksrc);
-        console.log(curentindex +"  " +songstracksrc.length)
-        
-        
-        if (curentindex<songstracksrc.length-1) {
-            curentindex++;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-        else{
-            curentindex=0;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-    })
+        curentindex = (curentindex + 1) % songstracksrc.length;
+        currtracksrc = songstracksrc[curentindex];
+        playsongs(currtracksrc);
+        changsongname(currtracksrc);
+    });
+
     prevsong.addEventListener("click", () => {
         let curentindex = songstracksrc.indexOf(currtracksrc);
-        console.log(curentindex +"  " +songstracksrc.length)
-
-        if (curentindex>0) {
-            curentindex--;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-        else{
-            curentindex=songstracksrc.length-1;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-
-    })
-};
-const playsongs = (currtracksrc) => {
-    
-    cuurentaudiotrack.src = currtracksrc;
-    cuurentaudiotrack.play();
-    playsong.src = "img/pause.svg"
+        curentindex = (curentindex - 1 + songstracksrc.length) % songstracksrc.length;
+        currtracksrc = songstracksrc[curentindex];
+        playsongs(currtracksrc);
+        changsongname(currtracksrc);
+    });
 }
 
+const playsongs = (currtracksrc) => {
+    cuurentaudiotrack.src = currtracksrc;
+    cuurentaudiotrack.play();
+    playsong.src = "img/pause.svg";
+};
 
 cuurentaudiotrack.addEventListener("timeupdate", () => {
     if (cuurentaudiotrack.currentTime == cuurentaudiotrack.duration) {
         let curentindex = songstracksrc.indexOf(currtracksrc);
-        console.log(curentindex +"  " +songstracksrc.length)
-        
-        
-        if (curentindex<songstracksrc.length-1) {
-            curentindex++;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-        else{
-            curentindex=0;
-            currtracksrc = songstracksrc[curentindex]
-            playsongs(currtracksrc)
-            changsongname(currtracksrc)
-        }
-        
+        curentindex = (curentindex + 1) % songstracksrc.length;
+        currtracksrc = songstracksrc[curentindex];
+        playsongs(currtracksrc);
+        changsongname(currtracksrc);
     }
 
-
     document.querySelector('.volumecontroller').innerHTML =
-    formatTime(cuurentaudiotrack.currentTime) + " / " + formatTime(cuurentaudiotrack.duration);
+        formatTime(cuurentaudiotrack.currentTime) + " / " + formatTime(cuurentaudiotrack.duration);
     let percent = cuurentaudiotrack.currentTime / cuurentaudiotrack.duration * 100;
-    console.log(percent)
-
     document.querySelector('#runningseekbar').style.width = percent + '%';
-    document.querySelector('.circle').style.left = percent+'%' ;
-
+    document.querySelector('.circle').style.left = percent + '%';
 });
 
 function formatTime(totalSeconds) {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = Math.floor(totalSeconds % 60);
-    const formattedTime = `${minutes}:${String(seconds).padStart(2, '0')}`;
-    return formattedTime;
+    return `${minutes}:${String(seconds).padStart(2, '0')}`;
 }
 
-
 document.querySelector(".seekbar").addEventListener("click", e => {
-
-    console.log(e.offsetX + "  width -->" + e.currentTarget.getBoundingClientRect().width);
     const boundingRect = e.currentTarget.getBoundingClientRect();
     const offsetX = e.clientX - boundingRect.left;
     const percent = (offsetX / boundingRect.width) * 100;
-    cuurentaudiotrack.currentTime = ((cuurentaudiotrack.duration) * percent) / 100
-
-})
-
+    cuurentaudiotrack.currentTime = ((cuurentaudiotrack.duration) * percent) / 100;
+});
 
 document.querySelector('.hamburger').addEventListener("click", () => {
     let rightvala = document.querySelector('.right');
     rightvala.style.display = "block";
     rightvala.style.position = "absolute";
-})
-document.querySelector('.closebutton').addEventListener("click",()=>{
-    document.querySelector('.right').style.display = "none";
-})
+});
 
+document.querySelector('.closebutton').addEventListener("click", () => {
+    document.querySelector('.right').style.display = "none";
+});
 
 function changsongname(src) {
-    let song = currtracksrc
-    song = song.split(`/songs/`)[1];
-    song = song.split(".mp3")[0];
-    song = song.replace(/%20/g, " ");
-    document.querySelector('.songinfoname').innerHTML = song.split("-")[0];
+    let song = currtracksrc.split(`/songs/`)[1].replace(".mp3", "").replace(/%20/g, " ");
+    document.querySelector('.songinfoname').innerHTML = song.split(" - ")[0];
 }
-
-
-
-
-
-// document.querySelector('.card').addEventListener("click", ()=>{
-//         console.log("working")
-//         if(songchange=="my"){
-//             songchange="yours"
-//             main("yours")
-//         }else{
-//             songchange="my";
-//             main("my")
-//         }
-//  });
